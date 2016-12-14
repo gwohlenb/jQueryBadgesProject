68eba6a908b0a60529555a0904471d9cffe1cdd2
Greg Wohlenberg <gwohlenb@yahoo.com>
2016-12-13 18:12:16 -0800

    Fixed location of AJAX call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/gregw1.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
