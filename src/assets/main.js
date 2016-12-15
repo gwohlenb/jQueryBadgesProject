79c3568d0d9b85ef8e4a0813f0269c55bf6cec40
Greg Wohlenberg <gwohlenb@yahoo.com>
2016-12-14 21:09:59 -0800

    Made additional DOM changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,19 @@
-$(function() {
+$(document).ready(function() {
 
   $.ajax({
     url: 'https://www.codeschool.com/users/gregw1.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+	for (key in response.courses.completed) {
+
+	  $("#badges").after("<div class='course'>")
+                      .after("<h3>" + response.courses.completed[key].title + "</h3>")
+                      .after("<img src=" + response.courses.completed[key].badge + ">")
+                      .after("<a class='btn btn-primary' target='_blank' href=" + response.courses.completed[key].url + ">See Course</a>")
+                      .after("</div>");  
+
+	  console.log("made additions to DOM");
+        }
     }
   });
-
 });
