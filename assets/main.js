fa18c04b3dfad8d2f7209612eec7f7400fd5dc25
Greg W <gregw1@iname.com>
2016-12-15 05:42:30 -0800

    Tried to fix the JS code

--- assets/main.js
+++ assets/main.js
@@ -4,16 +4,30 @@ $(document).ready(function() {
     url: 'https://www.codeschool.com/users/gregw1.json',
     dataType: 'jsonp',
     success: function(response) {
-	for (key in response.courses.completed) {
-
-	  $("#badges").after("<div class='course'>")
-                      .after("<h3>" + response.courses.completed[key].title + "</h3>")
-                      .after("<img src=" + response.courses.completed[key].badge + ">")
-                      .after("<a class='btn btn-primary' target='_blank' href=" + response.courses.completed[key].url + ">See Course</a>")
-                      .after("</div>");  
-
-	  console.log("made additions to DOM");
-        }
+      addTheCourses(response.courses.completed);
     }
-  });
+  }); 
+
+  function addTheCourses(coursesCompleted) { // Declared function
+
+    coursesCompleted.forEach(function(course) {
+
+      $("#badges");
+
+      var course = $('<div />', { 'class': 'course' })
+        .appendTo($('#badges'));
+
+      $('<h3 />', { text: course:title })
+        .appendTo($('#badges'));
+
+      $('<img />', { src: course:badge })
+        .appendTo($('#badges'));
+
+      $("<a />, { 'class': 'btn btn-primary', target: '_blank', href: 'text:url', text: 'See Course' })
+        .appendTo($('#badges'));
+
+       console.log("Add the courses");
+       return course;
+     }
+   }
 });
